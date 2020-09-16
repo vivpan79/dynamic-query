65ac572452149a4ea4e23cb78901d1fe7099bade
Vivek Pandit <vivek.pandit@axiomatics.com>
2020-09-16 20:25:44 +0200

    Use junit 5

--- src/test/java/com/telenor/dynamicquery/persistence/repository/ProductRepositoryTest.java
+++ src/test/java/com/telenor/dynamicquery/persistence/repository/ProductRepositoryTest.java
@@ -12,12 +12,12 @@ import com.telenor.dynamicquery.persistence.entity.Subscription;
 import java.math.BigDecimal;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = Application.class)
 class ProductRepositoryTest {
 
