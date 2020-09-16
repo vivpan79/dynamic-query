e1a9dbff2ee0e10b706eea90d0079ff0f4ece1f4
Vivek Pandit <vivek.pandit@axiomatics.com>
2020-09-16 11:05:01 +0200

    Add failing test for Spring boot application context load

---
+++ src/test/java/com/telenor/dynamicquery/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.telenor.dynamicquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
