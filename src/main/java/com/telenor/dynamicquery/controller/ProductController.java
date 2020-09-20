1083e0d716f2c58cc5953f4eb109a6c655b4cb91
Vivek Pandit <vivek.pandit@axiomatics.com>
2020-09-21 01:21:58 +0200

    Add Swagger support

--- src/main/java/com/telenor/dynamicquery/controller/ProductController.java
+++ src/main/java/com/telenor/dynamicquery/controller/ProductController.java
@@ -8,6 +8,8 @@ import com.telenor.dynamicquery.controller.error.InvalidProductTypeException;
 import com.telenor.dynamicquery.persistence.entity.Product;
 import com.telenor.dynamicquery.persistence.service.PhoneService;
 import com.telenor.dynamicquery.persistence.service.SubscriptionService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import java.math.BigDecimal;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,14 +32,26 @@ public class ProductController {
     }
 
     @GetMapping("/products")
+    @ApiOperation(
+        value = "Get Phone/Subscription information.",
+        notes = "Get Phone/Subscription information.",
+        response = ResponseData.class)
     public ResponseData findAllMatching(
+        @ApiParam(value = "Product type (phone/subscription)")
         @RequestParam(name = "productType", required = false) String type,
+        @ApiParam(value = "Minimum price of product")
         @RequestParam(name = "min_price", required = false) BigDecimal minPrice,
+        @ApiParam(value = "Maximum price of product")
         @RequestParam(name = "max_price", required = false) BigDecimal maxPrice,
+        @ApiParam(value = "City where store is located")
         @RequestParam(name = "city", required = false) String city,
+        @ApiParam(value = "Product property color/gd_limit. Note: Currently unused")
         @RequestParam(name = "property", required = false) String property,
+        @ApiParam(value = "Phone color")
         @RequestParam(name = "productProperty:color", required = false) String color,
+        @ApiParam(value = "Minimum data limit for subscription in GB")
         @RequestParam(name = "productProperty:gb_limit_min", required = false) Long minGBLimit,
+        @ApiParam(value = "Maximum data limit for subscription in GB")
         @RequestParam(name = "productProperty:gb_limit_max", required = false) Long maxGBLimit
     ) {
         List<? extends Product> products;
